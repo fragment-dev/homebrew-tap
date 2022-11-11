81e7ee9bc9cbfb4f374531c6f35268ad2adec2b2
Jerry Tsui <jerry@fragment.dev>
2022-11-11 15:52:52 -0500

    lol oops merge conflict

--- Formula/fragment-cli.rb
+++ Formula/fragment-cli.rb
@@ -3,16 +3,10 @@ require "language/node"
 class FragmentCli < Formula
   desc "The FRAGMENT CLI allows you to interact with the FRAGMENT API and develop your chart of accounts"
   homepage "https://fragment.dev"
-<<<<<<< Updated upstream
-  url "https://fragment-cli.s3.amazonaws.com/fragment-cli.tar.gz"
-  version "2022.11.9"
-  sha256 "0335dc5a17d13de6ee03b4354ba5b9d3c8199ceb2aa032511ed052a8bf6db396"
-=======
   # TODO: change this and the SHA to the newly uploaded ones after https://github.com/fragment-dev/workspaces/pull/1868 gets merged
   url "https://fragment-cli-test.s3.amazonaws.com/fragment-cli.tar.gz"
   version "2022.11.9.2"
   sha256 "92ebc56bf0b4f2df771c9b56f1115df73fde2771535f589b1791407e6f0d1b04"
->>>>>>> Stashed changes
   license "MIT"
   depends_on "node@16"
 

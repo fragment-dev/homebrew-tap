790ae270caa5e5e76141d4b7617d1799bfb93e91
Jerry Tsui <jerry@fragment.dev>
2022-11-14 08:39:16 -0500

    Build CLI in homebrew formula (#2)

--- Formula/fragment-cli.rb
+++ Formula/fragment-cli.rb
@@ -3,14 +3,17 @@ require "language/node"
 class FragmentCli < Formula
   desc "The FRAGMENT CLI allows you to interact with the FRAGMENT API and develop your chart of accounts"
   homepage "https://fragment.dev"
+  # TODO: change this and the SHA to the newly uploaded ones after https://github.com/fragment-dev/workspaces/pull/1868 gets merged
   url "https://fragment-cli.s3.amazonaws.com/fragment-cli.tar.gz"
-  version "2022.11.9.1"
-  sha256 "50016a7b4d86664b8db8a9e7d23f0a8a0ebfb4c7e59c02d6ff8d9760421ea09a"
+  version "2022.11.14"
+  sha256 "bba151f06b9639e1b7ef5f4c9d4f4009cb18fa83b851f007f0bf323cf3243d25"
   license "MIT"
   depends_on "node@16"
 
   def install
-    bin.install Dir["*"]
+    system "npm", "install"
+    system "npm", "run", "build"
+    bin.install Dir["dist/*"]
     man.mkpath
   end
 

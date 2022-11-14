6df1acf27efffea4d181ed4fd5eda545c864d2f6
Jerry Tsui <jerry@fragment.dev>
2022-11-14 14:27:00 -0500

    Add fragment-cli@next template

--- src/update-cli.ts
+++ src/update-cli.ts
@@ -1,5 +1,8 @@
 import { program } from '@caporal/core';
+import { execSync } from 'child_process';
 import fs from 'fs';
+import assert from 'assert';
+import tmp from 'tmp';
 import path from 'path';
 import { compile } from 'handlebars';
 
@@ -8,25 +11,65 @@ program
     required: true,
     validator: ['dev', 'prod'],
   })
-  .option('--shasum <shasum>', 'Shasum of the tarball', { required: true })
+  .option('--shasum <shasum>', 'Shasum of the tarball', {
+    required: true,
+    validator: program.STRING,
+  })
   .option('--cli-version <version>', 'CLI Version', {
     required: true,
+    validator: (val) => {
+      if (typeof val === 'string') {
+        if (!val.startsWith('v')) {
+          throw new Error(
+            `Prod release attemped with invalid release version: ${val}`
+          );
+        }
+        // strip 'v' prefix from GH release tag
+        return val.substring(1);
+      }
+      if (typeof val === 'number') {
+        // just some dummy
+        return `1.${val}.0`;
+      }
+      throw new Error('Invalid type');
+    },
   })
   .action(({ logger, options }) => {
-    const { stage, shasum, version } = options;
-    const outputPath = path.resolve('../Formula/fragment-cli.rb');
-    const templatePath = path.resolve('../templates/fragment-cli.rb');
+    const { stage, shasum, cliVersion } = options;
+    assert(cliVersion && typeof cliVersion === 'string');
+    const packageName = stage === 'dev' ? 'fragment-cli@next' : 'fragment-cli';
+    const url =
+      stage === 'prod'
+        ? 'https://fragment-cli.s3.amazonaws.com/fragment-cli.tar.gz'
+        : 'https://fragment-cli-dev.s3.us-west-2.amazonaws.com/fragment-cli.tar.gz';
+
+    const cwd = process.cwd();
+    process.chdir(tmp.dirSync({ keep: false }).name);
+    execSync(`curl -L ${url} --output graphite-cli.zip`);
+
+    const verifiedShasum = execSync('shasum -a 256 graphite-cli.zip')
+      .toString()
+      .trim()
+      .split(' ')[0];
+    logger.info(
+      `Shasum from package: ${verifiedShasum}. Shasum from webhook: ${shasum}`
+    );
+    if (verifiedShasum !== shasum) {
+      throw new Error(
+        `Mismatch in shasums. Shasum from package: ${verifiedShasum}. Shasum from webhook: ${shasum}`
+      );
+    }
+    process.chdir(cwd);
+
+    const outputPath = path.resolve(`./Formula/${packageName}.rb`);
+    const templatePath = path.resolve(`./templates/${packageName}.rb`);
     const updatedFormula = compile(fs.readFileSync(templatePath).toString())({
-      url:
-        stage === 'prod'
-          ? 'https://fragment-cli.s3.amazonaws.com/fragment-cli.tar.gz'
-          : 'https://fragment-cli-dev.s3.us-west-2.amazonaws.com/fragment-cli.tar.gz',
+      url,
       shasum,
-      version,
+      version: cliVersion,
     });
     logger.info(updatedFormula);
     fs.writeFileSync(outputPath, updatedFormula);
-    logger.info('hi');
   });
 
 program.run();
---
+++ templates/fragment-cli@next.rb
@@ -0,0 +1,22 @@
+require "language/node"
+
+class FragmentCli@next < Formula
+  desc "Beta versin of the FRAGMENT CLI"
+  homepage "https://fragment.dev"
+  url "{{url}}"
+  version "{{version}}"
+  sha256 "{{shasum}}"
+  license "MIT"
+  depends_on "node@16"
+
+  def install
+    system "npm", "install"
+    system "npm", "run", "build"
+    bin.install Dir["dist/*"]
+    man.mkpath
+  end
+
+  test do
+    raise "Test not implemented."
+  end
+end

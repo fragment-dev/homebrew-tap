b6d8fd635e9475b8538bdcbc3bd24a0abba6dc4a
peniqliotuv <peniqliotuv@users.noreply.github.com>
2022-11-17 23:12:46 +0000

    Update FRAGMENT CLI

--- Formula/fragment-cli.rb
+++ Formula/fragment-cli.rb
@@ -3,9 +3,9 @@ require "language/node"
 class FragmentCli < Formula
   desc "The FRAGMENT CLI allows you to interact with the FRAGMENT API and develop your chart of accounts"
   homepage "https://fragment.dev"
-  url "https://fragment-cli-prod.s3.us-west-2.amazonaws.com/fragment-cli.tar.gz?versionId=0M9mQZcqn72d..sVBEiehG3itAqSHHfv"
+  url "https://fragment-cli-prod.s3.us-west-2.amazonaws.com/fragment-cli-v2022.11.17.tar.gz"
   version "2022.11.17"
-  sha256 "1e1beefbfeb507682337d37768a222e20b931ef1b37610c1de367f544817a937"
+  sha256 "1d2d768d7ebcaaf55de9275f16490741636f2388e541491ea7e6ddd851c8c647"
   license "MIT"
   depends_on "node@16"
 

8764595e680ef7c9156c304d559c61294c4fcfbd
peniqliotuv <peniqliotuv@users.noreply.github.com>
2022-11-22 01:26:11 +0000

    Update FRAGMENT CLI

--- Formula/fragment-cli-beta.rb
+++ Formula/fragment-cli-beta.rb
@@ -3,9 +3,9 @@ require "language/node"
 class FragmentCliBeta < Formula
   desc "Beta version of the FRAGMENT CLI"
   homepage "https://fragment.dev"
-  url "https://fragment-cli-dev.s3.us-west-2.amazonaws.com/fragment-cli-v1399.tar.gz"
-  version "1399"
-  sha256 "8ccd684664a2c515ba016f583fe9730cfdcb263670474b64ad55ae4786b4019b"
+  url "https://fragment-cli-dev.s3.us-west-2.amazonaws.com/fragment-cli-v1420.tar.gz"
+  version "1420"
+  sha256 "4af2271cee620613e305eb7d645a80bd71add48d96ff108bc0069b38007d3adf"
   license "MIT"
   depends_on "node@16"
 

3a20f70fe92e767f9d6d4694af15eb11c6b37a03
Jerry Tsui <jerry@fragment.dev>
2022-11-21 19:01:28 -0800

    Test bumping the version and see if brew search shows it

--- Formula/fragment-cli.rb
+++ Formula/fragment-cli.rb
@@ -4,7 +4,7 @@ class FragmentCli < Formula
   desc "The FRAGMENT CLI allows you to interact with the FRAGMENT API and develop your chart of accounts"
   homepage "https://fragment.dev"
   url "https://fragment-cli-prod.s3.us-west-2.amazonaws.com/fragment-cli-v2022.11.17.tar.gz"
-  version "2022.11.18"
+  version "2022.11.19"
   sha256 "1d2d768d7ebcaaf55de9275f16490741636f2388e541491ea7e6ddd851c8c647"
   license "MIT"
   depends_on "node@16"

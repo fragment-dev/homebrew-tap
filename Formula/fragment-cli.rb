8bf2ff9e5f8ce4582c64466bff2576fd09ebf844
peniqliotuv <peniqliotuv@users.noreply.github.com>
2023-01-11 03:04:28 +0000

    Update FRAGMENT CLI in stage prod to version 2023.1.10

--- Formula/fragment-cli.rb
+++ Formula/fragment-cli.rb
@@ -3,15 +3,15 @@ require "language/node"
 class FragmentCli < Formula
   desc "The FRAGMENT CLI allows you to interact with the FRAGMENT API and develop your chart of accounts"
   homepage "https://fragment.dev"
-  url "https://prod-fragment-cli-bucket.s3.amazonaws.com/fragment-cli-v2022.12.29.tar.gz"
-  version "2022.12.29"
-  sha256 "b0a71528d22db70b7f38c804208e264a3a7b9ecf5364c18298f481ab8bd9eda1"
+  url "https://prod-fragment-cli-bucket.s3.amazonaws.com/fragment-cli-v2023.1.10.tar.gz"
+  version "2023.1.10"
+  sha256 "45cf4f02474b7ae3de72ce737545c473d96972734538929afeb2a1c20734c804"
   license "MIT"
   depends_on "node@16"
 
   def install
     system "npm", "install"
-    system "npm", "run", "build", "prod", "2022.12.29"
+    system "npm", "run", "build", "prod", "2023.1.10"
     bin.install Dir["dist/*"]
     man.mkpath
   end

923184468b3a3d2b3216f5b0c473388ff35cbe83
peniqliotuv <peniqliotuv@users.noreply.github.com>
2023-01-11 03:29:05 +0000

    Update FRAGMENT CLI in stage dev to version 1717.0.0

--- Formula/fragment-cli-beta.rb
+++ Formula/fragment-cli-beta.rb
@@ -3,15 +3,15 @@ require "language/node"
 class FragmentCliBeta < Formula
   desc "Beta version of the FRAGMENT CLI"
   homepage "https://fragment.dev"
-  url "https://dev-fragment-cli-bucket.s3.amazonaws.com/fragment-cli-v1655.0.0.tar.gz"
-  version "1655.0.0"
-  sha256 "12bce41c6857937df33da0a2bdb618d11e2dcee16ce08397b2427f6d77aebffc"
+  url "https://dev-fragment-cli-bucket.s3.amazonaws.com/fragment-cli-v1717.0.0.tar.gz"
+  version "1717.0.0"
+  sha256 "f346c30f66966eecf1cb6e30b1523fbc0e5e8cc479b39d1444e29b45309f61fe"
   license "MIT"
   depends_on "node@16"
 
   def install
     system "npm", "install"
-    system "npm", "run", "build", "dev", "1655.0.0"
+    system "npm", "run", "build", "dev", "1717.0.0"
     bin.install Dir["dist/*"]
     man.mkpath
   end

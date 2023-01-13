2bcd3605be1732e0e16ec968c795abd02ce1e0ce
Jerry Tsui <jerry@fragment.dev>
2023-01-13 09:47:29 -0500

    Try using packaged oclif CLI

--- Formula/fragment-cli-beta.rb
+++ Formula/fragment-cli-beta.rb
@@ -1,22 +1,19 @@
-require "language/node"
-
 class FragmentCliBeta < Formula
   desc "Beta version of the FRAGMENT CLI"
   homepage "https://fragment.dev"
-  url "https://dev-fragment-cli-bucket.s3.amazonaws.com/fragment-cli-v1729.0.0.tar.gz"
-  version "1729.0.0"
-  sha256 "d5fd153f62c7d769e21b242dc73e4e27687b64231cf9cbbbca562f8a829b632a"
+  url "https://dev-fragment-cli-bucket.s3.amazonaws.com/oclif-test"
+  version "test"
+  sha256 "dc757d1fa579b8dbc13cc52441b5d0b8828fa1688624226dccf9f15ca9f76c3c"
   license "MIT"
   depends_on "node@16"
 
   def install
-    system "npm", "install"
-    system "npm", "run", "build", "dev", "1729.0.0"
-    bin.install Dir["dist/*"]
-    man.mkpath
+    inreplace "bin/fragment", /^CLIENT_HOME=/, "export FRAGMENT_CLIENT_HOME=#{lib/"client"}\nCLIENT_HOME="
+    libexec.install Dir["*"]
+    bin.install_symlink libexec/"bin/fragment"
   end
 
   test do
-    raise "Test not implemented."
+    system bin/"fragment", "version"
   end
 end

56eb53001b295cafc11b4938b5e56259affda54f
peniqliotuv <peniqliotuv@users.noreply.github.com>
2023-01-19 21:32:56 +0000

    Update FRAGMENT CLI in stage dev to version 1775.0.0

--- Formula/fragment-cli-beta.rb
+++ Formula/fragment-cli-beta.rb
@@ -1,19 +1,22 @@
+require "language/node"
+
 class FragmentCliBeta < Formula
   desc "Beta version of the FRAGMENT CLI"
   homepage "https://fragment.dev"
-  url "https://dev-fragment-cli-bucket.s3.amazonaws.com/oclif-test"
-  version "test"
-  sha256 "dc757d1fa579b8dbc13cc52441b5d0b8828fa1688624226dccf9f15ca9f76c3c"
+  url "https://dev-fragment-cli-bucket.s3.amazonaws.com/fragment-cli-v1775.0.0.tar.gz"
+  version "1775.0.0"
+  sha256 "9a8e5b8558500bcdad8f66888bf4564fbb17d9a876d9ea3659337ca146b9eb5f"
   license "MIT"
   depends_on "node@16"
 
   def install
-    inreplace "bin/fragment", /^CLIENT_HOME=/, "export FRAGMENT_CLIENT_HOME=#{lib/"client"}\nCLIENT_HOME="
-    libexec.install Dir["*"]
-    bin.install_symlink libexec/"bin/fragment"
+    system "npm", "install"
+    system "npm", "run", "build", "dev", "1775.0.0"
+    bin.install Dir["dist/*"]
+    man.mkpath
   end
 
   test do
-    system bin/"fragment", "version"
+    raise "Test not implemented."
   end
 end

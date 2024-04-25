8c5034d3547038c53237af84108d7245e6c12d05
peniqliotuv <peniqliotuv@users.noreply.github.com>
2024-04-25 18:07:25 +0000

    Update FRAGMENT CLI in stage dev to version 4942.0.0

--- Formula/fragment-cli-beta.rb
+++ Formula/fragment-cli-beta.rb
@@ -4,18 +4,18 @@ class FragmentCliBeta < Formula
   desc "Beta version of the FRAGMENT CLI"
   homepage "https://fragment.dev"
   on_macos do
-    url "https://dev-fragment-cli-bucket.s3.amazonaws.com/fragment-cli-v4940.0.0-darwin-x64.tar.gz"
-    sha256 "50004db6a933a22f152401ce096e4490d3bfefbb4a52dfed52bcdb67439c9a29"
+    url "https://dev-fragment-cli-bucket.s3.amazonaws.com/fragment-cli-v4942.0.0-darwin-x64.tar.gz"
+    sha256 "4455803ae84d89226033fce4b8a95ded0e33db511db0387a5c8ab62b9af9ed8f"
     on_arm do
-      url "https://dev-fragment-cli-bucket.s3.amazonaws.com/fragment-cli-v4940.0.0-darwin-arm64.tar.gz"
-      sha256 "3c57e2aba156908655955570e690ec030b694ec507e06da71065d33efe34a8be"
+      url "https://dev-fragment-cli-bucket.s3.amazonaws.com/fragment-cli-v4942.0.0-darwin-arm64.tar.gz"
+      sha256 "b381a58f3d8e5c73814bbbcdffe616d482de0c900e99998a3b78f0e4ec84e0c8"
     end
   end
   on_linux do
-    url "https://dev-fragment-cli-bucket.s3.amazonaws.com/fragment-cli-v4940.0.0-linux-x64.tar.gz"
-    sha256 "ba512daca897d4fc5a8d33be3ce8bb7a86e548a0a49f186c17101cd8b7d9a7f9"
+    url "https://dev-fragment-cli-bucket.s3.amazonaws.com/fragment-cli-v4942.0.0-linux-x64.tar.gz"
+    sha256 "d607b5f53e7c3b2f9ded76af63e5b698fa9b7381996446f9293ad43c074e1880"
   end
-  version "4940.0.0"
+  version "4942.0.0"
   license "MIT"
   depends_on "node@20"
 
